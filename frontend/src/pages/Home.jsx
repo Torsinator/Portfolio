e1d42ac0fe1144caae3425b365afb013e3bfed7c
Tors <websteto@outlook.com>
2025-08-03 15:07:51 +1000

    Photos

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,12 +5,18 @@ import "../css/Home.css"
 
 function Home() {
     return <div className="homepage">
-        <img class="portrait-img" src={portraitImg} alt="Photograph of Tors Webster" width="600" />
-        <h1>Biography</h1>
-        <p>I am passionate about analog/digital electronics design, programming, control theory, machine learning and most other developments in the computing landscape.<br>
-        </br>
-        </p>
-        <EducationTile education={{photo: uonImg, description: "Hello"}}></EducationTile>
+        <h1 className="heading">Biography</h1>
+        <div className="biography">
+            <img class="portrait-img" src={portraitImg} alt="Photograph of Tors Webster" width="300" />
+            <div>
+                <p>I am passionate about analog/digital electronics design, programming, control theory, machine learning and most other developments in the computing landscape.
+                </p>
+                <p>I am currently completing my final year of my engineering degree
+                </p>
+            </div>
+        </div>
+        <h2 className="heading">Education</h2>
+        <EducationTile education={{ photo: uonImg, description: "Bachelor of Computer Systems Engineering (Honours) / Bachelor of Computer Science at the University of Newcastle" }}></EducationTile>
     </div>
 }
 
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,14 +1,18 @@
 import ProjectTile from "../components/ProjectTile"
+import IOT from "../images/IOT.png"
+import Protection_CCT from "../images/Protection_CCT.png"
+import Business_Sys from "../images/Business_Scheduler.png"
+import Medical_PCB from "../images/Medical_PCB.png"
 import "../css/Projects.css"
 
 function Projects()
 {
     const projects =
     [
-        {id: 1, title: "Business Invoicing System", date: "2020"},
-        {id: 2, title: "IOT Bedside Sensor System", date: "2025"},
-        {id: 3, title: "IOT Bedside Sensor System", date: "2025"},
-        {id: 4, title: "Medical Devices PCB", date: "2025"},
+        {id: 1, title: "Business Invoicing System / Scheduler", date: "2020", photo: Business_Sys},
+        {id: 2, title: "IOT Bedside Sensor System", date: "2025", photo: IOT},
+        {id: 3, title: "Over Voltage and Current Protection PCB", date: "2025", photo: Protection_CCT},
+        {id: 4, title: "Medical Devices PCB", date: "2025", photo: Medical_PCB},
         {id: 5, title: "Thesis - Data Driven Control With Reinforcement Learning", date: "2025"}
     ]
 
